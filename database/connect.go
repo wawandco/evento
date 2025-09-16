585e06af3fe073fb21fc133d1eea73b397876e86
Antonio Pagano <a@pagano.id>
2025-09-16 09:55:06 -0500

    task: tweaking some linting issues

--- database/connect.go
+++ database/connect.go
@@ -11,12 +11,12 @@ import (
 func Connect(url string) (*pgxpool.Pool, error) {
 	pconfig, err := pgxpool.ParseConfig(url)
 	if err != nil {
-		return nil, fmt.Errorf("Unable to parse DATABASE_URL: %w", err)
+		return nil, fmt.Errorf("unable to parse DATABASE_URL: %w", err)
 	}
 
 	conn, err := pgxpool.NewWithConfig(context.Background(), pconfig)
 	if err != nil {
-		return nil, fmt.Errorf("Unable to create connection pool: %w", err)
+		return nil, fmt.Errorf("unable to create connection pool: %w", err)
 	}
 
 	return conn, nil
